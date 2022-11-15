07e9f442271ee23cc0df79d4d5c5734a73f709c2
= <adedejifavour6@gmail.com>
2022-11-15 21:48:43 +0100

    Add store to index.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Profile from './components/profile';
+import Profile from './navigationPages/profile/profile';
+import Mission from './navigationPages/mission/missions';
 
 const App = () => (
   <Router>
     <Navbar />
     <Routes>
       <Route path="/" />
+      <Route path="/mission" element={<Mission />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   </Router>
