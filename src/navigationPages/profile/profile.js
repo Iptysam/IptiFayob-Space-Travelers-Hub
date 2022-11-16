8a135c39eb7ffb95efda16cc0adf487781a30246
= <adedejifavour6@gmail.com>
2022-11-16 12:08:26 +0100

    Add profile page

--- src/navigationPages/profile/profile.js
+++ src/navigationPages/profile/profile.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'react-bootstrap';
+import './profile.css';
 
-const Profile = () => (
-  <div>
-    Profile component
-  </div>
-);
+const Profile = () => {
+  const missionState = useSelector((state) => state.mission);
+
+  const reservedMission = missionState.filter((mission) => mission.reserved);
+
+  return (
+    <div className="profile_container">
+      <Col className="profile_column">
+        <h3>My Missions</h3>
+        {reservedMission.map((reserved) => (
+          <Row key={reserved.mission_id} className="profile_row">
+            <h5>{ reserved.mission_name }</h5>
+          </Row>
+        ))}
+      </Col>
+
+      <Col className="profile_column">
+        <h3>My Rockets</h3>
+      </Col>
+    </div>
+
+  );
+};
 
 export default Profile;
